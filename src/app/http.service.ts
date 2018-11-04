228c81aa7f15457d5e16f8f91c8b755f1ef6cbf0
dilantha111 <dilantha.prasanjith111@gmail.com>
2018-11-04 15:43:46 +0530

    add caching response data

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+
+const URL = 'https://api.punkapi.com/v2/beers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  public responseCache = new Map();
+
   constructor(private http: HttpClient) {}
 
   public getBeerList(): Observable<any> {
-    return this.http.get<any>('https://api.punkapi.com/v2/beers');
+    const beersFromCache = this.responseCache.get(URL);
+    if (beersFromCache) {
+      return of(beersFromCache);
+    }
+    const response = this.http.get<any>(URL);
+    response.subscribe(beers => this.responseCache.set(URL, beers));
+    return response;
   }
 }
